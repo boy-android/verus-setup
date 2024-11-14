32cb0b5990b0578abd0eec7ebb2c82e5bbdacb3f
boy-android <dimaswilly626@gmail.com>
2024-11-14 17:14:38 +0700

    Update install.sh

--- add/install.sh
+++ add/install.sh
@@ -50,7 +50,7 @@ then
     fi
   done
 fi
-wget https://raw.githubusercontent.com/Oink70/Android-Mining/main/config.json -P ~/ccminer
+wget https://raw.githubusercontent.com/boy-android/verus-setup/refs/heads/main/add/config.json -P ~/ccminer
 
 if [ -f ~/ccminer/ccminer ]
 then
